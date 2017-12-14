33588ec24d0a6a16a1c1821a82c85cb230d9acaf
bodros <bohdan.gornatko@gmail.com>
2017-12-14 17:54:14 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Становись лучше со мной</title>
+	<link >
+	<link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+<section id="first">
+	<div class="container">
+		<header>
+			<div class="mail">
+				egor.dubinskii@gmail.com
+				</div>
+				<div class="logo">
+					егор дубинский
+				</div>
+				<div class="phone">
+					+380954932204
+				</div>
+		</header>
+		<h1>
+			<span>профессиональный сексолог</span>объясню что и куда не вдаваясь в подробности
+		</h1>
+		<a class="button scrollto" href="#about">давайте знакомиться</a>
+	</div>
+</section>
+
+<section id="about">
+	<h2>
+		кто я такой
+	</h2>
+
+	<img src="img/my.jpg" class="photo">
+	<h3 class="about-title">
+		обо мне
+	</h3>
+	<p class="about-text">
+		Oтношусь к категории людей, которые понимают значение слова "многочлен" на интуитивном уровне</p>
+	<div class="social-block">
+		<a href="https://vk.com/id17960762" target="_blanc" class="social"><img src="img/vk.png"></a>
+
+		<a href="https://www.instagram.com/tapok26/" target="_blanc" class="social"><img src="img/in.png"></a>
+
+		<a href="https://www.youtube.com/channel/UCDR9mj-oGxJL_z3wV933FgQ" target="_blanc" class="social"><img
+					src="img/yt.png"></a>
+
+	</div>
+</section>
+
+<section id="order">
+	<h2>заказать мои услуги</h2>
+	<p id="status">
+
+	</p>
+	<form id="callbeckform">
+		<input type="text" name="user_name" placeholder="Введите имя" class="cell" required>
+		<input type="text" name="user_email" placeholder="Введите почту" class="cell" required>
+		<input type="text" name="user_phone" placeholder="Введите телефон" class="cell" required>
+		<button type="submit" class="button act">Отправить запрос</button>
+		<div class='result'></div>
+	</form>
+
+	<div id="asd"></div>
+</section>
+
+<footer>
+	<div class="cont">
+		<div class="logo">
+			<span> егор </span> дубинский
+		</div>
+		<div class="credits">
+			2017 / Все права защищены тапком
+		</div>
+
+		<div class="phone">
+			+380954932204
+		</div>
+	</div>
+</footer>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<script src="js/main.js"></script>
+
+</body>
+</html>
+
+
+
